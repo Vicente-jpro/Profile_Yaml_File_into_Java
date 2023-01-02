221fb7e11df5f5b4d2b4758a473520047d9d0bde
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-01-02 02:14:02 +0100

    Profile changed

--- src/main/java/com/example/demo/controller/Hello.java
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class Hello {
 	@Value("${system.name}")
 	private String valueFromApplicationPropertiesFile;
 	
+	@GetMapping("/hello")
 	public String hello() {
 		return valueFromApplicationPropertiesFile;
 	}
